497ffd27dc6e769a5e569bae0eddaf0132fff749
Erica-Ku <yeskuminji@naver.com>
2023-03-29 16:59:46 +0900

    (0329)

--- 코딩테스트 0단계.py
+++ 코딩테스트 0단계.py
@@ -9,3 +9,19 @@ def solution(n):
 # 배열의 평균값 - 함수는 따로 없음
 def solution(numbers):
     return sum(numbers)/len(numbers)
+
+# 점의 위치 구하기 - 논리 연산자(다른 언어와 혼동하면 안됨)
+def solution(dot):
+    if (dot[0]>0 and dot[1]>0):
+        return 1
+    elif (dot[0]<0 and dot[1]>0):
+        return 2
+    elif (dot[0]<0 and dot[1]<0):
+        return 3
+    else:
+        return 4
+
+# 배열 자르기 - slicing 사용
+def solution(numbers, num1, num2):
+    answer = numbers[num1 : num2+1]
+    return answer
