cb728853d9f1b3f89db7e29515389bde03871cca
Erica Ku (Ku Minji) <122212250+Erica-Ku@users.noreply.github.com>
2023-09-13 09:12:40 +0900

    Revert "0912"

--- NAHYEON/javascript/level0/배열의 평균값.js
+++
@@ -1,9 +0,0 @@
-function solution(numbers) {
-    var answer = 0;
-    var sum = 0;
-    for (let i = 0; i < numbers.length; i++) {
-        sum += numbers[i]
-    }
-    answer = sum / numbers.length;
-    return answer;
-}
--- NAHYEON/sql/나이 정보가 없는 회원 수 구하기.sql
+++
@@ -1,3 +0,0 @@
-SELECT count(USER_ID)
-from USER_INFO
-where age is null;
