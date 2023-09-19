a347603f90c44f3b2829caf17d81d964d6cf0175
Erica Ku (Ku Minji) <122212250+Erica-Ku@users.noreply.github.com>
2023-09-19 14:41:51 +0900

    0919

--- MINJI/java/Practice_0918_0922.java
+++ MINJI/java/Practice_0918_0922.java
@@ -38,3 +38,36 @@ class Solution {
         return answer;
     }
 }
+
+// 두 정수 사이의 합
+class Solution {
+    public long solution(int a, int b) {
+        long answer = 0;
+        int max = Math.max(a, b);
+        int min = Math.min(a, b);
+        for(int i = min ; i<=max; i++) {
+            answer += i;
+        }
+        return answer;
+    }
+}
+
+// 콜라츠 추측
+class Solution {
+    public int solution(long num) {
+        int answer = 0;
+        while(num != 1) {
+            if(num % 2 == 0)
+                num /= 2;
+            else
+                num = num * 3 + 1;
+            answer++;
+            
+            if(answer >= 500) {
+                answer = -1;
+                break;
+            }
+        }
+        return answer;
+    }
+}
