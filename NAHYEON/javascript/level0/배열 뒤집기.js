3f09e7a5547a02cf93bb5ef5a76b8e692e4a9284
n10531 <122756254+nh1531@users.noreply.github.com>
2023-09-21 10:16:26 +0900

    0921

---
+++ NAHYEON/javascript/level0/배열 뒤집기.js
@@ -0,0 +1,16 @@
+function solution(num_list) {
+  var answer = num_list.reverse();
+  return answer;
+}
+
+/**
+ * 
+function solution(num_list) {
+    var answer = [];
+    var j = num_list.length
+    for(var i = 1; i <= j; i++){
+        answer.push(num_list[j-i])
+    }
+    return answer;
+}
+ */
