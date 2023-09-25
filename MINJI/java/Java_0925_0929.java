86936e3d275e9b1ad008bfcd5653df29af8a41f3
Erica Ku (Ku Minji) <122212250+Erica-Ku@users.noreply.github.com>
2023-09-25 11:48:19 +0900

    0925

---
+++ MINJI/java/Java_0925_0929.java
@@ -0,0 +1,24 @@
+// 가운데 글자 가져오기
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        int length = s.length();
+        if(length % 2 == 0) {
+            answer = s.substring(length/2-1, length/2+1);
+        } else {
+            answer = s.substring(length/2, length/2+1);
+        }
+        return answer;
+    }
+}
+
+// 수박수박수박수박수박수?
+class Solution {
+    public String solution(int n) {
+        StringBuilder answer = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            answer.append((i % 2 == 0) ? "수" : "박");
+        }
+        return answer.toString();
+    }
+}
