011b012a92275d851a9389fcd5bdd57aa9b71b60
Erica Ku (Ku Minji) <122212250+Erica-Ku@users.noreply.github.com>
2023-09-26 11:41:21 +0900

    0926

--- MINJI/java/Java_0925_0929.java
+++ MINJI/java/Java_0925_0929.java
@@ -22,3 +22,29 @@ class Solution {
         return answer.toString();
     }
 }
+
+// 내적
+class Solution {
+    public int solution(int[] a, int[] b) {
+        int answer = 0;
+        for(int i = 0; i < a.length; i++) {
+            answer += a[i] * b[i];
+        }
+        return answer;
+    }
+}
+
+// 약수의 개수와 덧셈
+class Solution {
+    public int solution(int left, int right) {
+        int answer = 0;
+        for(int i = left; i <= right; i++){
+            int cnt = 0;
+            for(int j = 1; j <= i; j++){
+                if(i % j == 0) cnt++;
+            }
+            answer = (cnt % 2 == 0) ? answer + i : answer - i;
+        }
+        return answer;
+    }
+}
