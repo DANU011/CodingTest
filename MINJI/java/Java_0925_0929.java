1c4141de6fc215be16f8188757e10c39d48ff149
Erica Ku (Ku Minji) <122212250+Erica-Ku@users.noreply.github.com>
2023-09-27 15:34:51 +0900

    0927

--- MINJI/java/Java_0925_0929.java
+++ MINJI/java/Java_0925_0929.java
@@ -48,3 +48,34 @@ class Solution {
         return answer;
     }
 }
+
+// 문자열 내림차순으로 배치하기
+import java.util.Arrays;
+import java.util.Collections;
+
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        String[] str = s.split("");
+        Arrays.sort(str, Collections.reverseOrder());
+        for(String a : str)
+           answer += a;
+        return answer;
+    }
+}
+
+// 부족한 금액 계산하기
+class Solution {
+    public long solution(int price, int money, int count) {
+        long answer = 0;
+        long total = 0;
+        for(int i = 1; i < count + 1; i++){
+            total += price * i;
+        }
+        if(money < total)
+            answer = total - money;
+        else
+            answer = 0;
+        return answer;
+    }
+}
