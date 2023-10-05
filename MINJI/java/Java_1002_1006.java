11a182e69b8391c6354344dec540f49740ee8a7c
Erica Ku (Ku Minji) <122212250+Erica-Ku@users.noreply.github.com>
2023-10-05 11:25:53 +0900

    1002

--- MINJI/java/Java_1002_1006.java
+++ MINJI/java/Java_1002_1006.java
@@ -1,1 +1,31 @@
-// 
+// 같은 숫자는 싫어
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int []arr) {
+        ArrayList<Integer> answerList = new ArrayList<Integer>();
+        int value = -1;
+        for(int i = 0; i < arr.length; i++) {
+            if(arr[i] != value) {
+                answerList.add(arr[i]);
+                value = arr[i];
+            }
+        }
+        return answerList.stream().mapToInt(i->i).toArray();
+    }
+}
+
+// 3진법 뒤집기
+import java.util.*;
+
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        String ans = "";
+        while(n != 0) {
+            ans += n%3;
+            n /= 3;
+        }
+        return Integer.parseInt(ans, 3);
+    }
+}
