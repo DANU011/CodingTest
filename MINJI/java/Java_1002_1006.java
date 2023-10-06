4b48c7721c2392a933e2d7282977ea2243835cfb
Erica Ku (Ku Minji) <122212250+Erica-Ku@users.noreply.github.com>
2023-10-06 11:45:37 +0900

    200

--- MINJI/java/Java_1002_1006.java
+++ MINJI/java/Java_1002_1006.java
@@ -29,3 +29,35 @@ class Solution {
         return Integer.parseInt(ans, 3);
     }
 }
+
+// 예산
+import java.util.*;
+
+class Solution {
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+	    int total = 0;
+        Arrays.sort(d);
+        for(int i = 0; i < d.length; i++){
+            if(total + d[i] <= budget) {
+                total += d[i];
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
+
+// 이상한 문자 만들기
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        int cnt = 0;
+        String[] array = s.split("");
+        for(String ss : array) {
+            cnt = ss.contains(" ") ? 0 : cnt + 1;
+            answer += cnt%2 == 0 ? ss.toLowerCase() : ss.toUpperCase(); 
+        }
+        return answer;
+    }
+}
