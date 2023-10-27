5dfa58db43c472f6a9f992e8de933bae04f20e2a
Erica Ku (Ku Minji) <122212250+Erica-Ku@users.noreply.github.com>
2023-10-27 14:12:18 +0900

    1026

--- MINJI/java/Java_1023_1027.java
+++ MINJI/java/Java_1023_1027.java
@@ -125,3 +125,52 @@ class Solution {
         return answer;
     }
 }
+
+// 덧칠하기
+class Solution {
+    public int solution(int n, int m, int[] section) {
+        int roller = section[0];
+        int cnt = 1;
+        for(int i = 1; i < section.length; i++) {
+            if(roller + m - 1 < section[i]) {
+                cnt++;
+                roller = section[i];
+            }
+        }
+        return cnt;
+    }
+}
+
+// 실패율
+import java.util.*;
+
+class Solution {
+    public int[] solution(int N, int[] stages) {
+        int[] answer = new int[N];
+        int noclear = 0;
+        int player = stages.length;
+        Map<Integer, Double> stagefail = new HashMap<Integer, Double>();
+        for(int i = 1; i <= N; i++) {
+            for(int s : stages){
+                if(s == i) noclear++;
+            }
+            if(player == 0) stagefail.put(i, 0.0);
+            else {
+                stagefail.put(i, (double) noclear / player);
+                player -= noclear;
+                noclear = 0;
+            }
+        }
+        List<Map.Entry<Integer, Double>> list_entries = new ArrayList<Map.Entry<Integer, Double>>(stagefail.entrySet());
+        Collections.sort(list_entries, new Comparator<Map.Entry<Integer, Double>>() {
+            public int compare(Map.Entry<Integer, Double> obj1, Map.Entry<Integer, Double> obj2) {
+                return obj2.getValue().compareTo(obj1.getValue());
+            }
+        });
+        int c = 0;
+        for(Map.Entry<Integer, Double> entry : list_entries) {
+            answer[c] = entry.getKey(); c++;
+        }
+        return answer;
+    }
+}
