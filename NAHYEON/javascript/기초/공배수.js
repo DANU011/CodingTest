fddd5006d6966cc1c44c14c0157f5cadf6f89f09
n10531 <122756254+nh1531@users.noreply.github.com>
2023-11-02 23:28:15 +0900

    1102

--- NAHYEON/javascript/기초/공배수.js
+++ NAHYEON/javascript/기초/공배수.js
@@ -7,3 +7,15 @@ function solution(number, n, m) {
   }
   return 0;
 }
+
+// function solution(number, n, m) {
+//   return number % n === 0 && number % m === 0 ? 1 : 0
+// }
+
+// function solution(number, n, m) {
+//   if(number%n==0 && number%m==0){
+//       return 1;
+//   }
+
+//   return 0;
+// }
