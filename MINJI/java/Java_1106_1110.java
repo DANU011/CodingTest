b183cc829b56a5d7894f0579c55571be8215b552
Erica Ku (Ku Minji) <122212250+Erica-Ku@users.noreply.github.com>
2023-11-08 11:15:37 +0900

    1108

--- MINJI/java/Java_1106_1110.java
+++ MINJI/java/Java_1106_1110.java
@@ -51,3 +51,56 @@ class Solution {
         }
     }
 }
+
+// 체육복
+import java.util.*;
+
+class Solution {
+    public int solution(int n, int[] lost, int[] reserve) {
+        int answer = n - lost.length;
+        Arrays.sort(lost);
+        Arrays.sort(reserve);
+        for(int i = 0; i < lost.length; i++) {
+        	for(int j = 0; j < reserve.length; j++) {
+        		if(reserve[j] == lost[i]) {
+        			answer++;
+        			lost[i] = -1;
+        			reserve[j] = -1;
+        			break;
+        		}
+        	}
+        }
+        for(int i = 0; i < lost.length; i++) {
+        	for(int j = 0; j < reserve.length; j++) {
+        		if(reserve[j] == lost[i] - 1 || reserve[j] == lost[i] + 1) {
+        			answer++;
+        			lost[i] = -1;
+        			reserve[j] = -1;
+        			break;
+        		}
+        	}
+        }
+        return answer;
+    }
+}
+
+// 완주하지 못한 선수
+import java.util.*;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        HashMap<String, Integer> map = new HashMap<>();
+        for(String player : participant) map.put(player, map.getOrDefault(player, 0) + 1);
+        for(String player : completion) map.put(player, map.get(player) - 1);
+        Iterator<Map.Entry<String, Integer>> iter = map.entrySet().iterator(); 
+        while(iter.hasNext()) { 
+            Map.Entry<String, Integer> entry = iter.next(); 
+            if(entry.getValue() != 0) { 
+                answer = entry.getKey();
+                break; 
+            } 
+        } 
+        return answer;
+    }
+}
