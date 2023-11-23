0796458d683ea844357d103713b039274023147a
Erica Ku (Ku Minji) <122212250+Erica-Ku@users.noreply.github.com>
2023-11-23 14:23:55 +0900

    1123

--- MINJI/java/Java_1120_1124.java
+++ MINJI/java/Java_1120_1124.java
@@ -47,3 +47,55 @@ class Solution {
             .toString();
     }
 }
+
+// 바탕화면 정리
+class Solution {
+    public int[] solution(String[] wallpaper) {
+        int minRow, minCol;
+        int maxRow, maxCol;
+        minRow = minCol = Integer.MAX_VALUE;
+        maxRow = maxCol = Integer.MIN_VALUE;
+        int row = wallpaper.length;
+        int col = wallpaper[0].length();
+        for(int r = 0; r < row; r++) {
+            for(int c = 0; c < col; c++) {
+                if(wallpaper[r].charAt(c) == '#') {
+                    minRow = Math.min(minRow, r);
+                    minCol = Math.min(minCol, c);
+                    maxRow = Math.max(maxRow, r);
+                    maxCol = Math.max(maxCol, c);
+                }
+            }
+        }
+        return new int[]{minRow, minCol, maxRow + 1, maxCol + 1};
+    }
+}
+
+// 개인정보 수집 유효기간
+import java.util.*;
+
+class Solution {
+    public int[] solution(String today, String[] terms, String[] privacies) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        HashMap<String, Integer> map = new HashMap<>();
+        int date = getDate(today);
+        for(String s : terms) {
+            String[] term = s.split(" ");
+            map.put(term[0], Integer.parseInt(term[1]));
+        }
+        for(int i = 0; i < privacies.length; i++) {
+            String[] privacy = privacies[i].split(" ");
+            if(getDate(privacy[0]) + (map.get(privacy[1]) * 28) <= date) {
+                answer.add(i + 1);
+            }
+        }
+        return answer.stream().mapToInt(integer -> integer).toArray();
+    }
+    private int getDate(String today) {
+        String[] date = today.split("\\.");
+        int year = Integer.parseInt(date[0]);
+        int month = Integer.parseInt(date[1]);
+        int day = Integer.parseInt(date[2]);
+        return (year * 12 * 28) + (month * 28) + day;
+    }
+}
