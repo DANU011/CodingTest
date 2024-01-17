90a1f10e25bbfbae2d4082f7ceab493bc13c2396
DANU011 <122770856+DANU011@users.noreply.github.com>
2024-01-17 10:19:07 +0900

    240117_PYTHON_string

---
+++ DANU/python/10769.py
@@ -0,0 +1,10 @@
+message = input()
+
+if message.count(':-)') == 0 and message.count(':-(') == 0 :
+    print('none')
+elif message.count(':-)') == message.count(':-(') :
+    print('unsure')
+elif message.count(':-)') > message.count(':-(') :
+    print('happy')
+else :
+    print('sad')
