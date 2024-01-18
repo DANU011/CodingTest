90590dc4c0e27ba693b35fc6c769b157bacdd938
DANU011 <122770856+DANU011@users.noreply.github.com>
2024-01-18 20:39:25 +0900

    240118_PYTHON_string,

---
+++ DANU/python/2804.py
@@ -0,0 +1,19 @@
+word_A, word_B = input().split()
+rows, cols = len(word_A), len(word_B)
+
+puzzle = [['.'] * rows for _ in range(cols)]
+
+for i, char_A in enumerate(word_A) :
+    if char_A in word_B :
+        row = word_B.index(char_A)
+        col = i
+        break
+
+for i, char_A in enumerate(word_A) :
+    puzzle[row][i] = char_A
+
+for i, char_B in enumerate(word_B) :
+    puzzle[i][col] = char_B
+
+for row in puzzle :
+    print(''.join(row))
