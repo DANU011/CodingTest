3745f7e9ce961a47e862bf3e3e322fc9ecda5a7e
DANU011 <122770856+DANU011@users.noreply.github.com>
2024-01-27 00:21:48 +0900

    240127_PYTHON_string

--- DANU/python/27889.py
+++ DANU/python/27889.py
@@ -1,5 +1,5 @@
 school_names = ['North London Collegiate School', 'Branksome Hall Asia', 'Korea International School', 'St. Johnsbury Academy']
 abbreviations = ['NLCS', 'BHA', 'KIS', 'SJA']
-
 user_input = input()
+
 print(school_names[abbreviations.index(user_input)])
