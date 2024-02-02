8b980ff25a0a391670fd1900ba46b0782c603f57
Erica Ku (Ku Minji) <122212250+Erica-Ku@users.noreply.github.com>
2024-02-02 16:30:25 +0900

    0202

---
+++ MINJI/java/day2/BOJ1008.java
@@ -0,0 +1,12 @@
+package day2;
+
+import java.util.Scanner;
+
+public class BOJ1008 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        System.out.println(a / b);
+    }
+}
