06b30ec6e647d0c82b752b822bd6e93fd2cc0f08
DANU011 <122770856+DANU011@users.noreply.github.com>
2024-02-11 11:37:12 +0900

    240211_PYTHON_string

--- DANU/python/25640.py
+++ DANU/python/25640.py
@@ -2,7 +2,7 @@
 # 진호의 친구의 수 int
 # n개의 줄에 친구의 mbti 유형
 # 진호와 mbti 유형이 같은 사람의 수 출력
-def mbti_check(my_mbti, friends_mbti):
+def count_same_mbti(my_mbti, friends_mbti) :
     # count = 0
     # for mbti in friends_mbti:
     #     if mbti == my_mbti:
@@ -10,12 +10,18 @@ def mbti_check(my_mbti, friends_mbti):
     # return count
     return len([mbti for mbti in friends_mbti if mbti == my_mbti])
 
-
-if __name__ == "__main__":
+def get_friends_mbti() :
     friends_mbti = []
-    my_mbti = input()
-    for _ in range(int(input())):
+    num_friends = int(input())
+    for _ in range(num_friends) :
         mbti = input()
         friends_mbti.append(mbti)
+    return friends_mbti
 
-    print(mbti_check(my_mbti=my_mbti, friends_mbti=friends_mbti))
+def main() :
+    my_mbti = input()
+    friends_mbti = get_friends_mbti()
+    print(count_same_mbti(my_mbti=my_mbti, friends_mbti=friends_mbti))
+
+if __name__ == "__main__":
+    main()
