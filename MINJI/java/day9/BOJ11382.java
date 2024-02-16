bc33476286a4708635a65d622a7152f41beb2fc3
Erica Ku (Ku Minji) <122212250+Erica-Ku@users.noreply.github.com>
2024-02-16 15:56:17 +0900

    0216

---
+++ MINJI/java/day9/BOJ11382.java
@@ -0,0 +1,15 @@
+package day9;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class BOJ11382 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        BigInteger a = sc.nextBigInteger();
+        BigInteger b = sc.nextBigInteger();
+        BigInteger c = sc.nextBigInteger();
+
+        System.out.println((a.add(b)).add(c));
+    }
+}
---
+++ MINJI/java/day9/BOJ2743.java
@@ -0,0 +1,11 @@
+package day9;
+
+import java.util.Scanner;
+
+public class BOJ2743 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String text = sc.next();
+        System.out.println(text.length());
+    }
+}
