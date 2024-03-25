35478d44a63dd6d196be04528f3d701998c813e1
DANU011 <122770856+DANU011@users.noreply.github.com>
2024-03-25 16:50:26 +0900

    240325_PYTHON_string

---
+++ DANU/python/15312.py
@@ -0,0 +1,24 @@
+def calculate_love_percentage(A, B):
+    alpha = [3, 2, 1, 2, 3, 3, 2, 3, 3, 2, 2, 1, 2, 2, 1, 2, 2, 2, 1, 2, 1, 1, 1, 2, 2, 1]
+    dp = [[0 for _ in range(2*len(A))] for _ in range(2*len(A)-1)]
+
+    for i in range(len(A)+1):
+        if i != len(A): 
+            dp[0][i*2] = alpha[ord(A[i])-65]
+        if i != 0: 
+            dp[0][i*2-1] = alpha[ord(B[i-1])-65]
+
+    for i in range(1, 2*len(A)-1):
+        for j in range(2*len(A)-i):
+            dp[i][j] = int(str(dp[i-1][j] + dp[i-1][j+1])[-1])
+
+    return dp[-1][0], dp[-1][1]
+
+def main():
+    A = input()
+    B = input()
+    result = calculate_love_percentage(A, B)
+    print(f"{result[0]}{result[1]}")
+
+if __name__ == "__main__":
+    main()
